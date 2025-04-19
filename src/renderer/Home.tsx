eebb45826f8eaf067fbfe6c7fc468db4e0696899
yamaryosan <canda3267@gmail.com>
2025-04-19 11:44:28 +0900

    add: ホーム画面を作成

---
+++ src/renderer/Home.tsx
@@ -0,0 +1,11 @@
+import AnkiConnectCheck from './AnkiConnectCheck';
+import AllDecks from './AllDecks';
+
+export default function Home() {
+  return (
+    <>
+      <AnkiConnectCheck />
+      <AllDecks />
+    </>
+  );
+}
