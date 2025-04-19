e125c6e87c898a108a3f201e12a610090fddbf6c
yamaryosan <canda3267@gmail.com>
2025-04-19 17:50:30 +0900

    add: カード（ノート）の表面を表示

--- src/renderer/Deck.tsx
+++ src/renderer/Deck.tsx
@@ -2,41 +2,76 @@ import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 
+type NoteData = {
+  noteId: string;
+  profile: string;
+  tags: string[] | null;
+  modelName: string;
+  mod: number;
+  cards: number[];
+  fields: {
+    表面: {
+      order: number;
+      value: string;
+    };
+    裏面: {
+      order: number;
+      value: string;
+    };
+  };
+};
+
 /**
- * デッキ内のカードID一覧を取得する
+ * デッキ内のノートID一覧を取得する
  * @param deckname デッキ名
- * @returns カードID一覧
+ * @returns ノートID一覧
  */
-async function fetchCardIDsInDeck(deckname: string) {
-  const cardIDs = (await window.electron.ipcRenderer.invoke(
-    'fetch-card-ids-in-deck',
+async function fetchNoteIDsInDeck(deckname: string) {
+  const noteIDs = (await window.electron.ipcRenderer.invoke(
+    'fetch-note-ids-in-deck',
     deckname,
   )) as string[];
-  return cardIDs;
+  return noteIDs;
+}
+
+/**
+ * ノートID一覧からノートのデータを取得する
+ * @param noteIDs ノートID一覧
+ * @returns ノートのデータ一覧
+ */
+async function fetchNoteData(noteIDs: string[]) {
+  const noteData = (await window.electron.ipcRenderer.invoke(
+    'fetch-note-data',
+    noteIDs,
+  )) as NoteData[];
+  return noteData;
 }
 
 export default function Deck() {
   const { deckname } = useParams();
-  const [cards, setCards] = useState<string[]>([]);
+  const [notes, setNotes] = useState<NoteData[]>([]);
 
-  async function fetchCards() {
-    const cardIDs = await fetchCardIDsInDeck(deckname!);
-    setCards(cardIDs);
+  async function fetchNotes() {
+    const noteIDs = await fetchNoteIDsInDeck(deckname!);
+    const noteData = await fetchNoteData(noteIDs);
+    setNotes(noteData);
   }
 
   useEffect(() => {
-    fetchCards();
+    fetchNotes();
   }, [deckname]);
 
   return (
     <>
       <h2>デッキ: {deckname}</h2>
-      {cards.length === 0 ? (
+      {notes.length === 0 ? (
         <div>デッキ内にカードがありません</div>
       ) : (
         <ul>
-          {cards.map((card) => (
-            <li key={card}>{card}</li>
+          {notes.map((note) => (
+            <li key={note.noteId}>
+              <div>{note.fields.表面.value}</div>
+            </li>
           ))}
         </ul>
       )}
